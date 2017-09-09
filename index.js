2dd3cf96c5d5262af33765e9fe26267b1195f1e8
Arthur Gunn <arthur@gunn.co.nz>
2017-09-10 04:47:42 +1200

    Import react and output text.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("here!");
+import React from 'react'
+import {render} from 'react-dom'
+
+const App = ()=>
+  <h1>Here at {new Date().toLocaleDateString()}</h1>
+
+
+const element = document.getElementById('app')
+
+render(<App/>, element)
