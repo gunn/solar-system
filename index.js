f66aac5982a18449976ec2e41af565f1d78c672f
Arthur Gunn <arthur@gunn.co.nz>
2017-09-12 00:58:05 +1200

    Output an SVG.

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-const App = ()=>
-  <h1>Here at {new Date().toLocaleDateString()}</h1>
+const App = ({style})=>
+  <div>
+    <style>{style}</style>
+    <h1>Solar System</h1>
+
+    <SolarSystem />
+  </div>
+
+const PLANETS = []
+
+const SolarSystem = ()=> {
+  return (
+    <svg>
+      {
+        PLANETS.map((props, i)=> (
+          <Planet key={i} {...props}/>
+        ))
+      }
+    </svg>
+  )
+}
+
+const Planet = props=>
+  <g>
+    <Body />
+    <Orbit />
+  </g>
+
+const Body = ()=>
+  <circle className="planet" r={10} fill="#06f" />
+
+const Orbit = ()=>
+  <circle className="orbit" />
+
+
+const STYLES = `
+  html, body {
+    color: #CCC;
+    background-color: #000;
+  }
+`
 
 
 const element = document.getElementById('app')
-
-render(<App/>, element)
+render(<App style={STYLES}/>, element)
